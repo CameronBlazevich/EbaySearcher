eaedfc6afcb618d8717ec67e85d75fbe34389565
Cameron Blazevich <cameron.blazevich@gmail.com>
2016-09-30 21:10:36 -0500

    Rename second assembly because of funky name

--- EbaySearcherRepository.cs/Properties/AssemblyInfo.cs
+++ EbaySearcherRepository.cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EbaySearcherRepository.cs")]
+[assembly: AssemblyTitle("EbaySearcher.Repository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EbaySearcherRepository.cs")]
+[assembly: AssemblyProduct("EbaySearcher.Repository")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
